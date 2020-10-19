b23700a17e370d30fdd9103574607bd66b154ec3
“wllvps” <wzy.org.cn@gmail.com>
2020-10-20 02:18:53 +0800

    Site updated: 2020-10-20 02:18:53

---
+++ js/FootFish.js
@@ -0,0 +1,10 @@
+$(function(){
+// 初始化
+var myjs = new xkTool();
+// 背景修改
+myjs.bgPage();
+// 魔幻圆圈
+myjs.magicCirle([radius, densety, color, clearOffset]);
+// 页脚养鱼
+myjs.footFish();
+})
--- js/chocolate.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-/* 气泡+养鱼 */
-var chocolate = new xkTool();
-chocolate.magicCirle([radius, densety, color, clearOffset]);
-chocolate.footFish();
-})
